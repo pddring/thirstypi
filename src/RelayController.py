c6f74d2cdfc67f4869c1695c9c20ba666912cb95
Pete Dring <dringp@fulford.york.sch.uk>
2023-06-02 12:36:21 +0100

    Moved class to separate file

---
+++ src/RelayController.py
@@ -0,0 +1,17 @@
+class RelayController: 
+  def __init__(self, pins=[14, 15, 18, 23]):
+    self.relays = []
+    for pin in RELAY_PINS:
+      self.relays.append(gpiozero.OutputDevice(pin, active_high=True, initial_value=True))
+
+  def on(self, channel):
+    self.relays[channel].off()
+  
+  def off(self, channel):
+    self.relays[channel].on()
+
+  def set(self, channel, value):
+    if value:
+      self.relays[channel].off()
+    else:
+      self.relays[channel].on()
--- src/testrelay.py
+++ src/testrelay.py
@@ -1,31 +1,12 @@
 import time
 import gpiozero
+from RelayController import *
 print("Testing relays")
 RELAY_PINS = [14, 15, 18, 23]
 
 # Triggered by the output pin going high: active_high=True
 # Initially off: initial_value=False
-
-class RelayController: 
-  def __init__(self):
-    self.relays = []
-    for pin in RELAY_PINS:
-      self.relays.append(gpiozero.OutputDevice(pin, active_high=True, initial_value=True))
-
-  def on(self, channel):
-    self.relays[channel].off()
-  
-  def off(self, channel):
-    self.relays[channel].on()
-
-  def set(self, channel, value):
-    if value:
-      self.relays[channel].off()
-    else:
-      self.relays[channel].on()
-
-
-relays = RelayController()
+relays = RelayController(RELAY_PINS)
 
 for i in range(4):
   print("Relay", i, "on")
