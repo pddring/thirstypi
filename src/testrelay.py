096054b437227a545cdd23612aa19dc2385acc4f
Pete Dring <dringp@fulford.york.sch.uk>
2023-06-02 12:20:37 +0100

    Update testrelay.py

--- src/testrelay.py
+++ src/testrelay.py
@@ -1,1 +1,22 @@
+import time
+import gpiozero
 print("Testing relays")
+RELAY_PINS = [8, 10, 12, 16]
+
+# Triggered by the output pin going high: active_high=True
+# Initially off: initial_value=False
+
+relays = []
+for pin in RELAY_PINS:
+  relays.append(gpiozero.OutputDevice(pin, active_high=True, initial_value=False))
+  
+for i in range(4):
+  print("Relay, i, "off")
+  relays[i].off()
+  time.sleep(1)
+        
+  print("Relay, i, "on")
+  relays[i].on() # switch on
+  time.sleep(1)
+
+print("done")
